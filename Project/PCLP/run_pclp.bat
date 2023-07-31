7fb9f4c55e53197b8728ee18eb57d185ed03e9ca
Developer <isl2296@naver.com>
2023-07-31 18:42:42 +0900

    reset all

--- Project/PCLP/run_pclp.bat
+++ Project/PCLP/run_pclp.bat
@@ -1,4 +1,5 @@
 
-pclp64 -os=output.xml -i"C:\pclp\config" C:\pclp\config\co-gcc.lnt C:\pclp\lnt\au-misra3.lnt C:\VCAST\Lint\vectorcast_required.lnt C:\Demo\Src\*.c
-
 pclp64 -os=lint_results.txt -i"C:\pclp\config" C:\pclp\config\co-gcc.lnt C:\pclp\config\jenkins.lnt C:\pclp\lnt\au-misra3.lnt C:\Demo\Src\*.c
+
+cd C:\Demo\Project\PCLP
+pclp64 -os=output.xml -i"C:\pclp\config" C:\pclp\config\co-gcc.lnt C:\pclp\lnt\au-misra3.lnt C:\VCAST\Lint\vectorcast_required.lnt C:\Demo\Src\*.c
